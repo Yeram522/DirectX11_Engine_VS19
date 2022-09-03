47971bcc5f03ae563aa365e959cd93ddcfd6e20e
Marey <kamin_s2@g.hongik.ac.kr>
2022-09-04 03:23:28 +0900

    [CH1-9] Initialize DX11

---
+++ DirectX11 Engine VS19/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include "Engine.h"
+#include "Graphics/Graphics.h"
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	Engine engine;
+	engine.Initialize(hInstance, "Title", "MyWindowClass", 800, 600);
+
+	while (engine.ProcessMessages())
+	{
+		engine.Update();
+		engine.RenderFrame();
+	}
+	//S_OK :success    E_INVALIDARG-> parmameter incorrect
+	return 0;
+}
